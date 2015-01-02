1595e9aa9b4d4139036129dfeffa5076d8d0e184
enrique7mc <enrique7mc@gmail.com>
2015-01-01 19:22:36 -0600

    Añade clase ContactoTrabajo
    Subclase de Contacto

---
+++ ProyectoCSharpBasico/ContactoTrabajo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCSharpBasico
+{
+    public class ContactoTrabajo : Contacto
+    {
+        public string TelOficina { get; set; }
+
+        public ContactoTrabajo(string nombre, string telefono, string email, string telOficina) 
+            : base(nombre, telefono, email)
+        {
+            TelOficina = telOficina;
+        }        
+
+        public ContactoTrabajo(string telOficina)
+        {
+            TelOficina = telOficina;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Tel. Oficina: " + TelOficina;
+        }
+    }
+}
