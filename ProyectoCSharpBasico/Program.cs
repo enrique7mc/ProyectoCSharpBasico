5cfa2f76ab68a52a5762d4349eba5c09a35b134a
enrique7mc <enrique7mc@gmail.com>
2015-01-08 22:10:26 -0600

    Modifica Program
    Añade el menú principal

--- ProyectoCSharpBasico/Program.cs
+++ ProyectoCSharpBasico/Program.cs
@@ -8,27 +8,58 @@ namespace ProyectoCSharpBasico
 {
     class Program
     {
+        static ControlAgenda control = new ControlAgenda(new Agenda());
+
         static void Main(string[] args)
+        {            
+            string opcion = "";
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Agenda de Contactos");
+                ImprimeMenu();
+                opcion = Console.ReadLine();
+                switch(opcion)
+                {
+                    case "1":
+                        control.VerContactos();
+                        break;
+                    case "2":
+                        control.AgregarContacto();
+                        break;
+                    case "3":
+                        control.BorrarUltimoContacto();
+                        break;
+                    case "4":
+                        control.BuscarPorNombre();
+                        break;
+                    case "5":
+                        ControlAgenda.AcercaDe();
+                        break;
+                    case "6":
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
+
+
+            } while(opcion != "6");
+        }
+
+        static void ImprimeMenu()
         {
-            Contacto c1 = new Contacto("Pepe", "343213", "asd@asdsa.com");
-            Contacto c2 = new Contacto("Carlos", "45356", "asd@asdsa.com");
-            Contacto c3 = new Contacto("Tania", "54645", "asd@asdsa.com");
+            var sb = new StringBuilder();
+            sb.AppendLine("1. Ver Contactos");
+            sb.AppendLine("2. Agregar Contacto");
+            sb.AppendLine("3. Borrar Último Contacto");
+            sb.AppendLine("4. Buscar Contacto Por Nombre");
+            sb.AppendLine("5. Acerca De");
+            sb.AppendLine("6. Salir");
+            sb.Append("Seleccione una opción: ");
 
-            Agenda agenda = new Agenda();
-            agenda.AgregarContacto(c1);
-            agenda.AgregarContacto(c2);
-            agenda.AgregarContacto(c3);
-
-            agenda.MostrarOrdenadosDesc();
-            agenda.MostrarContactos();
-            Console.WriteLine(agenda.NumContactos);
-
-            agenda.BorrarUltimoContacto();
-
-            agenda.MostrarContactos();
-            //agenda.MostrarOrdenados();
-            //agenda.MostrarOrdenadosDesc();
-            Console.WriteLine(agenda.NumContactos);            
+            Console.Write(sb.ToString());
         }
     }
 }
